10780afcc434216b4b8778b7707bcedecbe76e62
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-06-04 22:08:54 +0600

    M2 Queue declare and find front value

---
+++ queue.cpp
@@ -0,0 +1,42 @@
+/* Author: Code__R
+   Email: mondalroudra212@gmail.com
+   Date  : 06 / 04 / 2025
+   Time  : 22:05
+   Problem Link: 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define YES cout << 'YES\n'
+#define Yes cout << 'Yes\n'
+#define NO cout << 'NO\n'
+#define No cout << 'No\n'
+#define ld long double
+#define pb push_back
+#define po pop_back
+#define all(v) v.begin(), v.end()
+#define pii pair<int,int>
+#define pll pair<<ll ,ll>
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve() {
+    // code here 
+    queue<int>q;
+    q.push(5);
+    q.push(3);
+    cout<<q.front()<<nl;
+}
+
+int main() {
+    fast_io;
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
